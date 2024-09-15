4a9d6b9954c36f2f1c6d0572470734f34a5c0b05
yumatan <yuma.tanaka1129@gmail.com>
2024-09-15 19:56:57 +0900

    use HookConsumerWidget

--- lib/location_page.dart
+++ lib/location_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class LocationPage extends StatelessWidget {
+class LocationPage extends HookConsumerWidget {
   const LocationPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      // TODO : request permission
+      print('LocationPage useEffect');
+      return null;
+    }, []);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Location Page'),
